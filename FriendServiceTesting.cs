9724936a40ada7b39ec8e238feecea71f1b6ff82
Savva <94635067+SiP7k@users.noreply.github.com>
2023-04-03 13:37:34 -0300

    Add files via upload

---
+++ FriendServiceTesting.cs
@@ -0,0 +1,29 @@
+﻿
+using NUnit.Framework;
+using SocialNetwork.Business_Logic_Layer.Models;
+using SocialNetwork.Business_Logic_Layer.Services;
+
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class FriendServiceTesting
+    {
+        [Test]
+        public void AddFriendShouldCreateFriendWithRightEmail()
+        {
+            UserService userService = new UserService();
+            FriendService friendService = new FriendService();
+
+            userService.Register(new UserRegistrationData { Email = "ca@gmail.com", Password = "savva123", FirstName = "Savva", LastName = "Chalcev" });
+            userService.Register(new UserRegistrationData { Email = "ca2@gmail.com", Password = "savva1234", FirstName = "Irma", LastName = "Chalceva" });
+           
+
+            
+            FriendRequestData friendData = new FriendRequestData { SenderId = 1, FriendEmail = "ca2@gmail.com" };
+            friendService.AddFriend(friendData);
+            Assert.That(friendService.GetFriendsList(1).First().Email == "ca2@gmail.com");
+        }
+    }
+    
+}
